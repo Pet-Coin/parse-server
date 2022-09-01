29e6cf7a40a4a6ca9ff2b1fcc137a47b2f9e0e9f
Davin <52686922+Frozies@users.noreply.github.com>
2022-09-01 12:13:59 -0400

    edit config

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const path = require('path');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
 
+if (!process.env.DATABASE_URI) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
 const config = {
   databaseURI: process.env.DATABASE_URI,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
