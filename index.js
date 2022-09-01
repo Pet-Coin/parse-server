6d65ea8509977df6cad2825d1076e9ac99381e69
Davin <52686922+Frozies@users.noreply.github.com>
2022-09-01 14:32:34 -0400

    feat: working server

--- index.js
+++ index.js
@@ -3,27 +3,16 @@
 
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
 const path = require('path');
-const {parseServer} = require("./src/parseServer");
-const {parseDashboard} = require("./src/parseDashboard");
+const Moralis = require('moralis');
+const {cleanEnv, num, str, bool} = require("envalid");
+const dotenv = require('dotenv');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
 
-if (!process.env.DATABASE_URI) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
 const port = process.env.PORT || 1337;
 
-const config = {
-  databaseURI: process.env.DATABASE_URI,
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || `http://localhost:${port}/parse`, // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
-  },
-};
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -32,9 +21,92 @@ const app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+function validateAuthData(authData) {
+  const { message, signature, network, id, authId } = authData;
+
+  return Moralis.Auth.verify({
+    message,
+    signature,
+    network,
+  })
+    .then(result => {
+      const data = result.toJSON();
+
+      if (id === data.profileId && authId === data.id) {
+        authData.chainId = result.result.chain.decimal;
+        authData.nonce = data.nonce;
+        authData.address = result.result.address.checksum;
+        authData.version = data.version;
+        authData.domain = data.domain;
+        authData.expirationTime = data.expirationTime;
+        authData.notBefore = data.notBefore;
+        authData.resources = data.resources;
+        authData.statement = data.statement;
+        authData.uri = data.uri;
+        return;
+      }
+
+      // @ts-ignore (see note at top of file)
+      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Moralis auth failed, invalid data');
+    })
+    .catch(() => {
+      // @ts-ignore (see note at top of file)
+      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Moralis auth failed, invalid data');
+    });
+}
+
+function validateAppId() {
+  return Promise.resolve();
+}
+dotenv.config();
+const config = cleanEnv(process.env, {
+  PORT: num(),
+  MORALIS_API_KEY: str(),
+
+  DATABASE_URI: str(),
+
+  CLOUD_PATH: str(),
+  APP_NAME: str(),
+
+  MASTER_KEY: str(),
+  APPLICATION_ID: str(),
+
+  ALLOW_INSECURE_HTTP: bool({ default: false }),
+});
+
+const SERVER_URL = `http://localhost:${port}/server`
 
 // Serve the Parse API on the /parse URL prefix
 if (!test) {
+  const parseServer = new ParseServer({
+    databaseURI: config.DATABASE_URI,
+    cloud: config.CLOUD_PATH,
+    appId: config.APPLICATION_ID,
+    masterKey: config.MASTER_KEY,
+    serverURL: SERVER_URL,
+    auth: {
+      moralis: {
+        module: {
+          validateAuthData,
+          validateAppId,
+        },
+      },
+    },
+  });
+  const parseDashboard = new ParseDashboard(
+    {
+      apps: [
+        {
+          appId: config.APPLICATION_ID,
+          masterKey: config.MASTER_KEY,
+          serverURL: SERVER_URL,
+          appName: config.APP_NAME,
+        },
+      ],
+    },
+    config.ALLOW_INSECURE_HTTP
+  );
+
   app.use('/server', parseServer);
   app.use('/dashboard', parseDashboard);
 }
--- src/config.ts
+++ src/config.ts
@@ -3,20 +3,4 @@ import { cleanEnv, num, str, bool } from 'envalid';
 
 dotenv.config();
 
-export default cleanEnv(process.env, {
-  PORT: num(),
-
-  MORALIS_API_KEY: str(),
-
-  DATABASE_URI: str(),
-
-  CLOUD_PATH: str(),
-  APP_NAME: str(),
-
-  SERVER_ENDPOINT: str(),
-  MASTER_KEY: str(),
-  APPLICATION_ID: str(),
-  SERVER_URL: str(),
-
-  ALLOW_INSECURE_HTTP: bool({ default: false }),
-});
+export default
