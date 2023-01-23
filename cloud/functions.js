1b708e13d8afa5f0a711d64fc083f2c7916067b0
Davin <52686922+Frozies@users.noreply.github.com>
2023-01-23 13:30:56 -0500

    test: setAcl on new user

--- cloud/functions.js
+++ cloud/functions.js
@@ -17,3 +17,15 @@ Parse.Cloud.define('getServerTime', (request) => {
   const dateToday = new Date()
   return dateToday.valueOf()
 })
+
+// Add admin read and write to a user on a new entry
+Parse.Cloud.beforeSave(Parse.User, async (request) => {
+    const user = request.object
+    const acl = new Parse.ACL()
+    acl.setRole(true)
+    acl.setRoleReadAccess('admin', true)
+    acl.setRoleWriteAccess('admin', true)
+    user.setACL(acl)
+    return user.save(null, { useMasterKey: true })
+    },
+)
